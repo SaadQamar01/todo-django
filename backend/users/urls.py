078c8641880d39e946d33dad51bd2775b1435205
mac <mac@macs-MacBook-Pro.local>
2024-07-22 17:35:12 +0500

    Remove all files and commit history

--- backend/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django import urls
-from .views import UserDetail, UserList, hellowWorld
-
-urlpatterns = [
-    path('users/', UserList.as_view(), name='Users'),
-    path('user/<int:pk>', UserDetail.as_view(),name='User'),
-]
